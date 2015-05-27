9ebfa67f0905843f2cfb4cbef391ebc0fe9d174e
Matthieu Bourgeois <mbo@itesoft.com>
2015-05-27 10:03:37 +0200

    Pushed docs to gh-pages

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Query",
+        "WMIResult"
+    ],
+    "modules": [
+        "wmi-query"
+    ],
+    "allModules": [
+        {
+            "displayName": "wmi-query",
+            "name": "wmi-query"
+        }
+    ]
+} };
+});
